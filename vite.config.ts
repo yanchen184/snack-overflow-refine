1434ac10bbfe4836180f8678dd421255d48b1b2e
yanchen184 <62279165+yanchen184@users.noreply.github.com>
2025-05-12 12:31:50 +0800

    添加 GitHub Actions 部署流程與 Vite 配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,11 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/snack-overflow-refine/",
+  build: {
+    outDir: "dist",
+  },
 });
